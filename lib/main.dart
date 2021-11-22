5fbc57f999c3a235a5964e3dcb2539b0f22711bd
Nikita Vyzhol <flamenv22@gmail.com>
2021-11-22 18:52:58 +0200

    Complete my weather app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_weather_app/weather_bloc.dart';
+import 'package:flutter_weather_app/weather_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather_app/weather_repository.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,105 +15,215 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Colors.grey[900],
+          body: BlocProvider(
+            create: (BuildContext context) => WeatherBloc(WeatherRepo()),
+            child: const SearchPage(),
+          ),
+        ));
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final weatherBloc = BlocProvider.of<WeatherBloc>(context);
+    var cityController = TextEditingController();
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: SizedBox(
+            child: Image.asset(
+              "assets/earth.png",
+              fit: BoxFit.contain,
+            ),
+            height: 300,
+            width: 300,
+          ),
+        ),
+        BlocBuilder<WeatherBloc, WeatherState>(
+          builder: (context, state) {
+            if (state is WeatherIsNotSearched) {
+              return Container(
+                padding: const EdgeInsets.only(
+                  left: 32,
+                  right: 32,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      "Check the weather",
+                      style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white70),
+                    ),
+                    const Padding(padding: EdgeInsets.only(top: 24)),
+                    TextFormField(
+                      controller: cityController,
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.white70,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                                color: Colors.white70,
+                                style: BorderStyle.solid)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                                color: Colors.blue, style: BorderStyle.solid)),
+                        hintText: "Enter your city",
+                        hintStyle: TextStyle(color: Colors.white70),
+                      ),
+                      style: const TextStyle(color: Colors.white70),
+                    ),
+                    const Padding(padding: EdgeInsets.only(top: 24)),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                        ),
+                        onPressed: () {
+                          weatherBloc.add(FetchWeather(cityController.text));
+                        },
+                        child: const Text(
+                          "Search",
+                          style: TextStyle(color: Colors.white70, fontSize: 16),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            } else if (state is WeatherIsLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is WeatherIsLoaded) {
+              return ShowWeather(state.getWeather, cityController.text);
+            } else {
+              return const Text(
+                "Error",
+                style: TextStyle(color: Colors.white),
+              );
+            }
+          },
+        )
+      ],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class ShowWeather extends StatelessWidget {
+  WeatherModel weather;
+  final city;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  ShowWeather(this.weather, this.city, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      padding: const EdgeInsets.only(right: 32, left: 32, top: 10),
+      child: Column(
+        children: [
+          Text(city,
+              style: const TextStyle(
+                  color: Colors.white70,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold)),
+          Text(weather.getTemp.round().toString() + "C",
+              style: const TextStyle(color: Colors.white70, fontSize: 50)),
+          const Text(
+            "Temperature",
+            style: TextStyle(color: Colors.white70, fontSize: 14),
+          ),
+          const Padding(padding: EdgeInsets.only(top: 20)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    weather.getMinTemp.round().toString() + "C",
+                    style: const TextStyle(color: Colors.white70, fontSize: 30),
+                  ),
+                  const Text(
+                    "Min Temperature",
+                    style: TextStyle(color: Colors.white70, fontSize: 14),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    weather.getMaxTemp.round().toString() + "C",
+                    style: const TextStyle(color: Colors.white70, fontSize: 30),
+                  ),
+                  const Text(
+                    "Max Temperature",
+                    style: TextStyle(color: Colors.white70, fontSize: 14),
+                  ),
+                ],
+              )
+            ],
+          ),
+          const Padding(padding: EdgeInsets.only(top: 20)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    weather.getPressure.round().toString() + " mm Hg",
+                    style: const TextStyle(color: Colors.white70, fontSize: 30),
+                  ),
+                  const Text(
+                    "Pressure",
+                    style: TextStyle(color: Colors.white70, fontSize: 14),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    weather.humidity.toString() + "%",
+                    style: const TextStyle(color: Colors.white70, fontSize: 30),
+                  ),
+                  const Text(
+                    "Humidity",
+                    style: TextStyle(color: Colors.white70, fontSize: 14),
+                  ),
+                ],
+              )
+            ],
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/weather_bloc.dart
@@ -0,0 +1,64 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_weather_app/weather_model.dart';
+import 'package:flutter_weather_app/weather_repository.dart';
+
+class WeatherEvent extends Equatable {
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class FetchWeather extends WeatherEvent {
+  final _city;
+
+  FetchWeather(this._city);
+
+  @override
+  List<Object> get props => [_city];
+}
+
+class ResetWeather extends WeatherEvent {}
+
+class WeatherState extends Equatable {
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class WeatherIsNotSearched extends WeatherState {}
+
+class WeatherIsLoading extends WeatherState {}
+
+class WeatherIsLoaded extends WeatherState {
+  final _weather;
+
+  WeatherIsLoaded(this._weather);
+
+  WeatherModel get getWeather => _weather;
+
+  @override
+  List<Object> get props => [_weather];
+}
+
+class WeatherIsNotLoaded extends WeatherState {}
+
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  WeatherRepo weatherRepo;
+
+  WeatherBloc(this.weatherRepo) : super(WeatherIsNotSearched());
+
+  @override
+  Stream<WeatherState> mapEventToState(WeatherEvent event) async* {
+    if (event is FetchWeather) {
+      yield WeatherIsLoading();
+
+      try {
+        WeatherModel weather = await weatherRepo.getWeather(event._city);
+        yield WeatherIsLoaded(weather);
+      } catch (_) {
+        yield WeatherIsNotLoaded();
+      }
+    } else if (event is ResetWeather) {
+      yield WeatherIsNotSearched();
+    }
+  }
+}
